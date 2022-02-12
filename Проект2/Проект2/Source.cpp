a0412bb1ea65d334c02727b0a5eafd773b305e86
Mihail856 <49765745+Mihail856@users.noreply.github.com>
2022-02-12 17:25:51 +0300

    Add files via upload

---
+++ Проект2/Проект2/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+#include <limits>
+
+using namespace std;
+
+const int N = 80;
+
+void ob(char* mas, int* ras)
+{
+
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char 
+	int ras;
+	ob(&mas,&ras)
+	system("pause");
+	return 0;
+}
